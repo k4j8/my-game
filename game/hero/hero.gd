1e1e67ad62dd71c59557ee2b8828f9ce62ede1e7
Kyle Johnston <johnstonkylew@gmail.com>
2017-11-29 21:14:52 -0500

    Added grid system
    Added a grid system that the hero moves within. Enemies are randomly placed and generated.

--- game/hero/hero.gd
+++ game/hero/hero.gd
@@ -1,30 +1,57 @@
 extends KinematicBody2D
 
-export (int) var SPEED
+var direction = Vector2()
+
+const SPEED = 100
+
 var velocity = Vector2()
 
-func _ready():
-	set_process(true)
+var target_pos = Vector2()
+var target_direction = Vector2()
+var is_moving = false
 
-func _process(delta):
-	var position = get_pos()
-	velocity = Vector2()
-	
-	if Input.is_action_pressed("ui_right"):
-		velocity.x += 1
-	if Input.is_action_pressed("ui_left"):
-		velocity.x -= 1
-	if Input.is_action_pressed("ui_down"):
-		velocity.y += 1
+var type
+var grid
+
+
+func _ready():
+	grid = get_parent()
+	type = grid.HERO
+	set_fixed_process(true)
+
+
+func _fixed_process(delta):
+	direction = Vector2() # resets direction
+
 	if Input.is_action_pressed("ui_up"):
-		velocity.y -= 1
-	if velocity.length() > 0:
-		velocity = velocity.normalized() * SPEED
-	
-	var motion = velocity * delta
-	motion = move(motion)
-	if (is_colliding()):
-		var n = get_collision_normal()
-		motion = n.slide(motion)
-		velocity = n.slide(velocity)
-		move(motion)
+		direction.y = -1
+	elif Input.is_action_pressed("ui_down"):
+		direction.y = 1
+
+	if Input.is_action_pressed("ui_left"):
+		direction.x = -1
+	elif Input.is_action_pressed("ui_right"):
+		direction.x = 1
+
+	if not is_moving and direction != Vector2():
+
+		# Initialize moving
+		target_direction = direction.normalized()
+		if grid.is_cell_vacant(get_pos(), direction):
+			target_pos = grid.update_child_pos(get_pos(), direction, type)
+			is_moving = true
+
+	elif is_moving:
+
+		# Prepare to stop moving if target will be reached
+		var move_distance = velocity.length()
+		var distance_to_target = get_pos().distance_to(target_pos)
+		if move_distance > distance_to_target:
+			velocity = target_direction * distance_to_target
+			is_moving = false
+		else:
+			# Get vector to start or continue moving
+			velocity = SPEED * target_direction * delta
+
+		# Move
+		move(velocity)
---
+++ game/scenes/grid.gd
@@ -0,0 +1,66 @@
+# Collection of functions to work with a Grid. Stores all its children in the grid array
+extends TileMap
+
+enum ENTITY_TYPES {HERO, enemy, COLLECTIBLE}
+
+var tile_size = get_cell_size()
+var half_tile_size = tile_size / 2
+var grid_size = Vector2(16, 16)
+
+var grid = []
+onready var enemy = preload("res://enemy/enemy.tscn")
+onready var hero = preload("res://hero/hero.tscn")
+
+func _ready():
+	for x in range(grid_size.x):
+		grid.append([])
+		for y in range(grid_size.y):
+			grid[x].append(null)
+
+	# Place hero in world and grid
+	var new_hero = hero.instance()
+	new_hero.set_pos(map_to_world(Vector2(11,12)) + half_tile_size)
+	add_child(new_hero)
+
+	# Place enemies in world and grid
+	var positions = []
+	for x in range(5):
+		var placed = false
+		while not placed:
+			var grid_pos = Vector2(randi() % int(grid_size.x), randi() % int(grid_size.y))
+			if not grid[grid_pos.x][grid_pos.y]:
+				if not grid_pos in positions:
+					positions.append(grid_pos)
+					placed = true
+
+	for pos in positions:
+		var new_enemy = enemy.instance()
+		new_enemy.set_pos(map_to_world(pos) + half_tile_size)
+		grid[pos.x][pos.y] = new_enemy.get_name()
+		add_child(new_enemy)
+
+
+func get_cell_content(pos=Vector2()):
+	return grid[pos.x][pos.y]
+
+
+func is_cell_vacant(pos=Vector2(), direction=Vector2()):
+	var grid_pos = world_to_map(pos) + direction
+
+	if grid_pos.x < grid_size.x and grid_pos.x >= 0:
+		if grid_pos.y < grid_size.y and grid_pos.y >= 0:
+			return true if grid[grid_pos.x][grid_pos.y] == null else false
+	return false
+
+
+func update_child_pos(new_pos, direction, type):
+	# Remove node from current cell, add it to the new cell, returns the new target move_to position
+	var grid_pos = world_to_map(new_pos)
+	print(grid_pos)
+	grid[grid_pos.x][grid_pos.y] = null
+	
+	var new_grid_pos = grid_pos + direction
+	grid[new_grid_pos.x][new_grid_pos.y] = type
+	
+	var target_pos = map_to_world(new_grid_pos) + half_tile_size
+	return target_pos
