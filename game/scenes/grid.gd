7f23125f84a01ffebce80b622342a549c168d467
Kyle Johnston <johnstonkylew@gmail.com>
2017-12-11 19:50:10 -0500

    Hero and enemy placement moved from script to GUI

--- game/scenes/grid.gd
+++ game/scenes/grid.gd
@@ -11,18 +11,19 @@ onready var hero = preload("res://hero/hero.tscn")
 
 
 func _ready():
+	pass
 
-	# Place hero in world
-	var new_hero = hero.instance()
-	new_hero.set_pos(map_to_world(Vector2(2,2)) + half_tile_size)
-	add_child(new_hero)
+#	# Place hero in world
+#	var new_hero = hero.instance()
+#	new_hero.set_pos(map_to_world(Vector2(2,2)) + half_tile_size)
+#	add_child(new_hero)
 
-	# Place enemies in world
-	var positions = [ Vector2(4,4), Vector2(4,8), Vector2(8,4), Vector2(2,8), Vector2(10,14), Vector2(10,16), Vector2(10,18), Vector2(6,14), Vector2(18,2), Vector2(16,6), Vector2(18,14), Vector2(12,4), Vector2(18,10), Vector2(22,8) ]
-	for pos in positions:
-		var new_enemy = enemy.instance()
-		new_enemy.set_pos(map_to_world(pos) + half_tile_size)
-		add_child(new_enemy)
+#	# Place enemies in world
+#	var positions = [ Vector2(4,4) ]
+#	for pos in positions:
+#		var new_enemy = enemy.instance()
+#		new_enemy.set_pos(map_to_world(pos) + half_tile_size)
+#		add_child(new_enemy)
 
 
 func update_child_pos(new_pos, direction, type):
