4d93663fc7a161e4def8f42c388e50db7846be8d
Kyle Johnston <johnstonkylew@gmail.com>
2017-12-14 23:31:21 -0500

    Add win sound effect

---
+++ game/scenes/win_area.gd
@@ -0,0 +1,22 @@
+extends Area2D
+
+var hero_count = 1
+
+
+func _ready():
+	connect("body_enter", self, "win")
+
+
+func win(body):
+	# Play win sound effect
+	get_tree().get_root().get_node("World").get_node("Sounds").play("win")
+	
+	# Start timer to reset game
+	var timer = Timer.new()
+	timer.connect("timeout",self,"_on_timer_timeout") 
+	add_child(timer)
+	timer.start()
+
+
+func _on_timer_timeout():
+	get_tree().reload_current_scene()
